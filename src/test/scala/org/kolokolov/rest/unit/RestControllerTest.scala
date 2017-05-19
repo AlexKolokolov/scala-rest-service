6d6a0be9df508c0435e8870682ba59ed1e42ec05
AlexKolokolov <kolokolov_a@ukr.net>
2017-05-19 16:42:38 +0300

    Add mocked unit tests for RestController

---
+++ src/test/scala/org/kolokolov/rest/unit/RestControllerTest.scala
@@ -0,0 +1,78 @@
+package org.kolokolov.rest.unit
+
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.kolokolov.model.{Comment, Message, User}
+import org.kolokolov.rest.{JsonSupport, RestController}
+import org.kolokolov.service.{CommentService, MessageService, UserService}
+import org.scalamock.scalatest.AsyncMockFactory
+import org.scalatest.{AsyncFunSuite, Matchers}
+
+import scala.concurrent.Future
+
+/**
+  * Created by Kolokolov on 11.05.2017.
+  */
+class RestControllerTest extends AsyncFunSuite
+  with AsyncMockFactory
+  with Matchers
+  with ScalatestRouteTest
+  with JsonSupport {
+
+  private val firstUser = User("Bob Marley",1)
+  private val secondUser = User("Tam Waits",2)
+
+  private val firstMessage = Message("Hello, everybody!",1,1)
+  private val secondMessage = Message("Happy New Year!",2,2)
+
+  private val firstComment = Comment("Hello, everybody!",1,1,1)
+  private val secondComment = Comment("Happy New Year!",2,2,2)
+
+  def createRoute: Route = {
+    val stubUserService = stub[UserService]
+    (stubUserService.getUserById _).when(1).returns(Future(Some(firstUser)))
+    (stubUserService.getAllUsers _).when().returns(Future(Seq(firstUser,secondUser)))
+    val stubMessageService = stub[MessageService]
+    (stubMessageService.getMessageById _).when(1).returns(Future(Some(firstMessage)))
+    (stubMessageService.getAllMessages _).when().returns(Future(Seq(firstMessage,secondMessage)))
+    val stubCommentService = stub[CommentService]
+    (stubCommentService.getCommentById _).when(1).returns(Future(Some(firstComment)))
+    (stubCommentService.getAllComments _).when().returns(Future(Seq(firstComment,secondComment)))
+    new RestController(stubUserService, stubMessageService, stubCommentService, system).rootRoute
+  }
+
+  test("should return all users") {
+    val route = createRoute
+    Get("/webapi/users") ~> route ~> check {
+      responseAs[Seq[User]] shouldEqual Seq(firstUser,secondUser)
+    }
+  }
+
+  test("should return firstUser") {
+    val route = createRoute
+    Get("/webapi/users/1") ~> route ~> check {
+      responseAs[User] shouldEqual firstUser
+    }
+  }
+
+  test("should return all messages") {
+    val route = createRoute
+    Get("/webapi/messages") ~> route ~> check {
+      responseAs[Seq[Message]] shouldEqual Seq(firstMessage,secondMessage)
+    }
+  }
+
+  test("should return firsMessage") {
+    val route = createRoute
+    Get("/webapi/messages/1") ~> route ~> check {
+      responseAs[Message] shouldEqual firstMessage
+    }
+  }
+
+  test("should return all comments") {
+    val route = createRoute
+    Get("/webapi/comments") ~> route ~> check {
+      responseAs[Seq[Comment]] shouldEqual Seq(firstComment,secondComment)
+    }
+  }
+}
