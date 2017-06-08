4d2f9906fcb1698a38103bc48757c5056ab2cb25
AlexKolokolov <kolokolov_a@ukr.net>
2017-06-08 14:47:57 +0300

    Replace condition with require in OrderItem

--- src/main/scala/org/kolokolov/model/OrderItem.scala
+++ src/main/scala/org/kolokolov/model/OrderItem.scala
@@ -4,5 +4,5 @@ package org.kolokolov.model
   * Created by Kolokolov on 24.05.2017.
   */
 case class OrderItem(orderId: Int, productId: Int, quantity: Int, id: Int = 0) extends Identifiable {
-  if (quantity <= 0) throw new IllegalArgumentException("Quantity should be positive")
+  require(quantity > 0, "Quantity should be positive")
 }
